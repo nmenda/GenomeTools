edba9255a5973eca0013790963e479199a1e3645
nmenda <naana.menda@cornell.edu>
2013-11-14 14:59:23 -0500

    fix bin counting , and add check for the last introgression in wach chromosome

--- introgression/introgression_regions.pl
+++ introgression/introgression_regions.pl
@@ -9,17 +9,20 @@ my $density =10;
 my $bins = 5;
 my $spaces = 4;
 my $out_file = "introgressions.tab";
- 
+my $skip;
+
 GetOptions (
     "infile=s"   => \$tab_file,   # string
     "density=i"  => \$density,    # numeric
     "bins=i"     => \$bins,       # flag
     "space=i"    => \$spaces,
-    "out=s"        => \$out_file   
+    "out=s"      => \$out_file,
+    "c_skip=s"     => \$skip # chromosome ID to skip
     );
+
 print "Density = $density, bins = $bins, spaces = $spaces, out= $out_file \n\n";
 my @lines = read_file($tab_file);
-my $bin_count = 1;
+my $bin_count = 0;
 my $introg_count = 1;
 my $space_count;
 my @first = split("\t" , $lines[0] ) ;
@@ -28,15 +31,20 @@ my %data;
 
 foreach my $line (@lines) {
     chomp $line;
-    my ($chr, $loc , $snps, $coverage, $shared1, $ref2, $shared2, $ref3, $ref4 ) = split ("\t" , $line ); #  1 = pimpinellifolium, 2 = YP , 3 = BTI87, 4= pimpin.
-    chomp ($chr, $loc, $snps, $coverage, $shared1, $ref2, $shared2, $ref3, $ref4 );
-    #if ($chr eq "SL2.40ch00") { next ; }
+    my @intersect  = split ("\t" , $line ); #  1 = pimpinellifolium, 2 = YP , 3 = BTI87, 4= pimpin.
+    
+    my $chr = $intersect[0];
+    if ( $chr eq $skip ) { next; }
+    my $loc = $intersect[1];
+    my $snps = $intersect[2];
+    my $coverage = $intersect[3];
+
     if (  $chr eq $prev_chr  ) {
 	###print "snps = $snps ";
 	if ( $snps < $density ) { $space_count ++ ; }
 	##print " space count = $space_count ";
 	if ( $snps >= $density  || $space_count <= $spaces  || ( $space_count > $spaces && $coverage < 50000 ) ) {
-	    push( @{ $data{$introg_count} }, "$chr\t$loc\t$snps\t$coverage\t$shared1\t$ref2\t$shared2\t$ref3\t$ref4" );
+	    push( @{ $data{$introg_count} },  join ("\t" , @intersect ) );
 	   ### print " BIN count = $bin_count \n";
 	    if ($snps >= $density ) { 
 		$space_count = 0 ; 
@@ -49,7 +57,7 @@ foreach my $line (@lines) {
 	undef $data{ $introg_count } ;
 	####print " NO INTROGRESSION. snps = $snps, bins = $bin_count , spaces = $spaces_count \n";
 	$introg_count++;
-	$bin_count = 1;
+	$bin_count = 0;
     } elsif (  ( $snps < $density ) && ( $bin_count >= $bins ) && ( ( $space_count > $spaces ) && ( $coverage > 50000 ) )  ) { #end of introgression when # of SNPs bellow threshold and number spaces has maxed, and coverage is sufficient 
 	#####print "Moving to next introgression :  snps = $snps, bins = $bin_count , spaces = $spaces_count \n";
 	print "End of introgression $introg_count. bins = $bin_count, spaces = $space_count\n";
@@ -59,26 +67,31 @@ foreach my $line (@lines) {
 	}
         ##
 	$introg_count++;
-	$bin_count = 1;
+	$bin_count = 0;
 	$space_count = 0;
     }
     # change of chromosome
     if ( $chr ne $prev_chr ) { 
 	print "Next chromosome : $chr\n\n";
+	#check the last introgression of the previous chromosome
+	if ( $bin_count < $bins ) { undef $data{ $introg_count } ;  }
 	$prev_chr = $chr;
-	$bin_count = 1;
+	$bin_count = 0;
 	$space_count = 0;
 	$introg_count++;
-	push( @{ $data{$introg_count} }, "$chr\t$loc\t$snps\t$coverage\t$shared1\t$ref2\t$shared2\t$ref3\t$ref4" );
+	push( @{ $data{$introg_count} }, join ("\t", @intersect )  );
 	if ( $snps < $density ) { $space_count++; } 
 	$bin_count++;
     }
 }
-
+my $i_num = 0;
 foreach my $key (sort { $a <=> $b}  keys %data) {
-    if (defined ($data{$key} ) ) { print "Writing introgression $key...\n"; }
+    if (defined ($data{$key} ) ) { 
+	$i_num++;
+	print "Writing introgression $key ($i_num)...\n"; 
+    }
     foreach my $bin ( @{ $data{$key} } ) {
-	write_file( $out_file, {append => 1 }, ($key , "\t",  $bin, "\n") ) ;
+	write_file( $out_file, {append => 1 }, ($i_num , "\t",  $bin, "\n") ) ;
     }
 }
 
